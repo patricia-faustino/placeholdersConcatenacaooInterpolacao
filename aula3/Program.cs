1fe070c7575bad1b8e7e609e153754f2bb9f4606
Patricia <patriciafaustinobr@gmail.com>
2020-09-27 19:29:25 -0300

    Implementando placeholders, concatenacao e interpolacao

---
+++ aula3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace aula3 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+            Console.WriteLine("{0} tem {1} anos e seu saldo eh {2:F2} reais",nome, idade, saldo);
+            Console.WriteLine($"{nome} tem {idade} anos e seu saldo eh {saldo:F2} reais");
+            Console.WriteLine(nome+ "tem "+ idade+ " anos e seu saldo eh " + saldo.ToString("F2") + " reais");
+        }
+    }
+}
